473f936213b79f20bc006811e9004a6636ebb636
gabriel carlos <gabrielcarlos91@hotmail.com>
2020-09-21 14:50:42 -0300

    Configurado Mainwindow

--- AcademyDota2Lobby/AcademyDota2Lobby/MainWindow.xaml.cs
+++ AcademyDota2Lobby/AcademyDota2Lobby/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AcademyDota2Lobby.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +25,45 @@ namespace AcademyDota2Lobby
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation(0, 1,
+                                    (Duration)TimeSpan.FromSeconds(1));
+            this.BeginAnimation(UIElement.OpacityProperty, animation);
+
+            SwitchScreen(new UCHome());
+        }
+
+        private void BtnSettings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        /// <summary>
+        /// Switch Screen Selecionada
+        /// </summary>
+        /// <param name="sender">UserControl</param>
+        internal void SwitchScreen(object sender)
+        {
+            var screen = ((UserControl)sender);
+
+            if (screen != null)
+            {
+                StackPanelMain.Children.Clear();
+                StackPanelMain.Children.Add(screen);
+            }
         }
     }
 }
